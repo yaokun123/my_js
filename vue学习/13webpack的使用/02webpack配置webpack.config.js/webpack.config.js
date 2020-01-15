f50f172b0f7c55799fb7e59ee36013cce65336f4
yaok <yaok@pingansec.com>
2020-01-15 23:26:24 +0800

    02webpack配置webpack.config.js

---
+++ vue学习/13webpack的使用/02webpack配置webpack.config.js/webpack.config.js
@@ -0,0 +1,18 @@
+//自己手动创建这个文件
+
+
+const path = require('path');
+//最好在目录下执行npm init生成package.json
+
+
+
+//配置文件以commonJS方式
+module.exports = {
+    entry:'./src/main.js',
+    output:{
+
+        //这个路径是绝对路径
+        path:path.resolve(__diename,'dist'),
+        filename:'bundle.js'
+    }
+};
