5b9094e5adbe8e6303a3a6cb71d80b24179ae487
yaokun <yaok@pingansec.com>
2020-01-19 17:07:27 +0800

    vue cli3

--- vue学习/13webpack的使用/testvuecli3/src/main.js
+++ vue学习/13webpack的使用/testvuecli3/src/main.js
@@ -6,3 +6,15 @@ Vue.config.productionTip = false
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
+
+//这里的mount其实与之前的el一样，el就是调用mount
+
+//如何修改vue-cli3下的配置文件
+//一、通过vue ui命令启动图形化界面，在图形化界面中修改
+
+//二、node_modules > @vue > cli-service > webpack.config.js
+//                                      >lib > Service.js
+//                                      >lib > config
+
+//三、在项目目录下创建vue.config.js
