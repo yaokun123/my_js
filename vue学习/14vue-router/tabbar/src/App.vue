fb8fc177a480440a7c3cba4bd51a5553cf5699a8
yaok <yaok@pingansec.com>
2020-01-20 23:04:25 +0800

    tabbar

---
+++ vue学习/14vue-router/tabbar/src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="app">
+    <tab-bar>
+      <tab-bar-item>
+        <img slot="item-icon" src="./assets/img/tabbar/home.png" alt="">
+        <div slot="item-text">
+          首页
+        </div>
+      </tab-bar-item>
+      <tab-bar-item>
+        <img slot="item-icon" src="./assets/img/tabbar/category.png" alt="">
+        <div slot="item-text">
+          分类
+        </div>
+      </tab-bar-item>
+      <tab-bar-item>
+        <img slot="item-icon" src="./assets/img/tabbar/shopcart.png" alt="">
+        <div slot="item-text">
+          购物车
+        </div>
+      </tab-bar-item>
+      <tab-bar-item>
+        <img slot="item-icon" src="./assets/img/tabbar/profile.png" alt="">
+        <div slot="item-text">
+          我的
+        </div>
+      </tab-bar-item>
+    </tab-bar>
+  </div>
+</template>
+
+<script>
+  import TabBar from './components/tabbar/TabBar'
+  import TabBarItem from './components/tabbar/TabBarItem'
+export default {
+  name: 'App',
+  components:{
+    TabBar,
+    TabBarItem
+  }
+}
+</script>
+
+<style>
+  /*导入基础样式*/
+  @import "assets/css/base.css";
+
+</style>
---
+++ vue学习/14vue-router/tabbar/src/components/tabbar/TabBarItem.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="tab-bar-item">
+    <slot name="item-icon">
+
+    </slot>
+    <slot name="item-text">
+
+    </slot>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "TabBarItem"
+    }
+</script>
+
+<style scoped>
+  .tab-bar-item{
+    flex: 1;
+    text-align: center;
+    height: 49px;
+    font-size: 14px;
+  }
+  .tab-bar-item img{
+    margin-top: 3px;
+    width: 24px;
+    height: 24px;
+    vertical-align: middle;
+  }
+</style>
