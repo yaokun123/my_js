02d7eb5f3750c13b3840a3a1b8077a27f8a2cd5c
yaokun <yaok@pingansec.com>
2020-01-21 11:00:52 +0800

    tabbar添加vue-router

--- vue学习/14vue-router/tabbar/src/App.vue
+++ vue学习/14vue-router/tabbar/src/App.vue
@@ -4,25 +4,25 @@
       <tab-bar-item>
         <img slot="item-icon" src="./assets/img/tabbar/home.png" alt="">
         <div slot="item-text">
-          首页
+          <router-link to="/home">首页</router-link>
         </div>
       </tab-bar-item>
       <tab-bar-item>
         <img slot="item-icon" src="./assets/img/tabbar/category.png" alt="">
         <div slot="item-text">
-          分类
+          <router-link to="/category">分类</router-link>
         </div>
       </tab-bar-item>
       <tab-bar-item>
         <img slot="item-icon" src="./assets/img/tabbar/shopcart.png" alt="">
         <div slot="item-text">
-          购物车
+          <router-link to="/shopcart">购物车</router-link>
         </div>
       </tab-bar-item>
       <tab-bar-item>
         <img slot="item-icon" src="./assets/img/tabbar/profile.png" alt="">
         <div slot="item-text">
-          我的
+          <router-link to="/profile">我的</router-link>
         </div>
       </tab-bar-item>
     </tab-bar>
--- vue学习/14vue-router/tabbar/src/router/index.js
+++ vue学习/14vue-router/tabbar/src/router/index.js
@@ -3,7 +3,29 @@ import Router from 'vue-router'
 
 Vue.use(Router)
 
+//const Laze = () => import('@/components/Laze')
+import Home from '@/components/tabbar/Home'
+import Profile from '@/components/tabbar/Profile'
+import Category from '@/components/tabbar/Category'
+import Shopcart from '@/components/tabbar/Shopcart'
+
 export default new Router({
   routes: [
+    {
+      path:'/home',
+      components:Home
+    },
+    {
+      path:'/profile',
+      components:Profile
+    },
+    {
+      path:'/category',
+      components:Category
+    },
+    {
+      path:'/shopcart',
+      components:Shopcart
+    }
   ]
 })
